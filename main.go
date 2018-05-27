636c9246a01b86937194bcb40a24129f99f216aa
Alex <brilalex1989@gmail.com>
2018-05-27 23:31:08 +0300

    github.com/gorilla/mux. Update .gitignore

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
-package main
+ package main
 
 import (
 	"net/http"
 	"fmt"
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	r := mux.NewROuter()
+
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
